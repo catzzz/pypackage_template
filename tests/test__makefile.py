893520c4f663f613613a1d9d93c5bde80a53559f
Catzzz <chuneleu@gmail.com>
2024-03-14 12:33:43 -0700

    add test__linting

---
+++ tests/test__makefile.py
@@ -0,0 +1,27 @@
+"""Test Makefile.
+
+1. generate a project using the cookiecutter
+2. create a virtual environment and install project deppendencies
+---
+3. run tests
+4. run linting
+---
+Cleanu up / Tear down
+5. remove the virtual environment
+6. remove the generated project
+
+"""
+import subprocess
+from pathlib import Path
+
+
+def test__linting_passes(project_dir: Path):
+    subprocess.run(["make", "lint"], cwd=project_dir, check=True)
+
+
+def test__tests_pass():
+    ...
+
+
+def test__install_succeeds():
+    ...
