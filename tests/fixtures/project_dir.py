95a14c3df33939ac9829b524c78513eff30df615
Catzzz <chuneleu@gmail.com>
2024-03-14 13:52:05 -0700

    implement pytest-xdist

--- tests/fixtures/project_dir.py
+++ tests/fixtures/project_dir.py
@@ -4,6 +4,7 @@
 import shutil
 import subprocess
 from pathlib import Path
+from uuid import uuid4
 
 import pytest
 from utils.project import (
@@ -12,13 +13,14 @@ from utils.project import (
 )
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="session")
 def project_dir() -> Path:
     """Create an instance of our cookiecutter template to be re-used in tests."""
+    test_session_id:str = generate_test_session_id()
+    template_values = {"repo_name": f"test-repo-{test_session_id}"}
 
-    template_values = {"repo_name": "test-repo"}
-    print("Setup")
-    generated_repo_dir = generate_project(template_values=template_values)
+    generated_repo_dir = generate_project(template_values=template_values, 
+                                          test_session_id=test_session_id)
     try:
         initialize_git_repo(generated_repo_dir)
         subprocess.run(["make", "lint-ci"], cwd=generated_repo_dir, check=False)
@@ -26,3 +28,9 @@ def project_dir() -> Path:
     finally:
         shutil.rmtree(path=generated_repo_dir)
         print("Teardown")
+
+
+def generate_test_session_id() -> str:
+    """Generate a unique session id for the test session."""
+    test_session_id = str(uuid4())[:6]
+    return test_session_id
--- tests/utils/project.py
+++ tests/utils/project.py
@@ -21,11 +21,11 @@ def initialize_git_repo(repo_dir: Path):
 
 
 
-def generate_project(template_values: Dict[str, str]):
+def generate_project(template_values: Dict[str, str], test_session_id: str):
     """Generate a project using the cookiecutter."""
     template_values = deepcopy(template_values)
     cookiecutter_config = {"default_context": template_values}
-    cookiecutter_config_file = PROJECT_DIR / "cookiecutter-config.json"
+    cookiecutter_config_file = PROJECT_DIR / f"cookiecutter-{test_session_id}.json"
     cookiecutter_config_file.write_text(json.dumps(cookiecutter_config))
     cmd = [
         "cookiecutter",
